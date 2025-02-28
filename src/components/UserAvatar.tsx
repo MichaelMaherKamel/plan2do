70da7386ce00e72bcdfde22ccc2c6f7ae8847c2d
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2025-03-01 01:53:50 +0200

    DB&AUTH Setup

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,13 +1,16 @@
 import { auth } from '../auth'
+import Image from 'next/image'
 
 export default async function UserAvatar() {
   const session = await auth()
-
   if (!session?.user) return null
+
+  // Add a fallback image path
+  const imageSrc = session.user.image || '/default-avatar.png'
 
   return (
     <div>
-      <img src={session.user.image} alt='User Avatar' />
+      <Image src={imageSrc} alt='User Avatar' width={40} height={40} />
     </div>
   )
 }
